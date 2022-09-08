0c3f36eebae3e5e574b932582b4517f389a58b0a
Gabriel Azenha Fachim <101106957+GabrielAF-Faca@users.noreply.github.com>
2022-09-08 10:10:28 -0300

    Add files via upload

---
+++ desafio/Form1.cs
@@ -0,0 +1,61 @@
+namespace desafio
+{
+    public partial class Form1 : Form
+    {
+        List<Aluno> alunos;
+        List<string> cursos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            alunos = new List<Aluno>();
+            cursos = new List<string>();
+        }
+
+
+        private void button_listar_Click(object sender, EventArgs e)
+        {
+
+            textBox_listagem.Text = "";
+
+
+
+            foreach (Aluno aluno in alunos)
+                textBox_listagem.Text += "Nome: " + aluno.get_nome() + " - Curso: " + aluno.get_curso() + "\r\n";
+
+        }
+
+        private void button_cadastrar_Click(object sender, EventArgs e)
+        {
+            Aluno aluno = new Aluno(textBox_nome.Text, textBox_curso.Text);
+
+            alunos.Add(aluno);
+            cursos.Add(textBox_curso.Text);
+
+            Util.comb_sort(cursos, alunos);
+
+            textBox_nome.Text = "";
+            textBox_curso.Text = "";
+
+
+        }
+
+        private void atualizar_button(object sender, EventArgs e)
+        {
+            if (textBox_nome.Text.Length > 0 && textBox_curso.Text.Length > 0)
+                button_cadastrar.Enabled = true;
+            else
+                button_cadastrar.Enabled = false;
+
+
+        }
+
+        
+
+        private void button_limpar_Click(object sender, EventArgs e)
+        {
+            textBox_listagem.Text = "";
+
+        }
+    }
+}
